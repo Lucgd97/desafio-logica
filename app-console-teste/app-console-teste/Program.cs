fc2322222eba383faa866ba053d99082715d86cc
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-01-08 11:50:49 -0300

    Desafio 1 de logica

---
+++ app-console-teste/app-console-teste/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// mostre() // mostrar uma mensagem na tela
+// leia() // ler algo que o usuario digita
+//csharp é dinamico (var), forte tipagem,
+//data type c# = ver tipos primitivos da linguagem
+// ? = nullabel
+// sempre quesitonar o problema e pedir exemplo
+// vc identificar string(classe ou primitivo) e tals é mais rapido para compilar ao inves de utilizar "var" pq espera a respota para identificar a varaivel 
+
+Console.WriteLine("========================================="); // mostre
+Console.WriteLine("=== Bem vindo ai meu primeiro sistema ==="); // mostre
+Console.WriteLine("========================================="); // mostre
+
+Console.WriteLine("Digite algo:"); // mostre()
+var informacao = Console.ReadLine(); // leia() // vem acompanhado em armazenar(variavel) "setar algo"
+
+Console.WriteLine($"""
+    O valor que você digitou foi: {informacao}
+    """);
+
+Console.WriteLine("=== [Finalizando Sistema] ===");
---
+++ app-console-teste/exercicio-log-1/Program.cs
@@ -0,0 +1,53 @@
+﻿
+//Minha resolução desafio 1
+
+//Console.WriteLine("=== Bem vindo ao sistema de Cadastro de nome e Sobrenome (1.0) ==="); 
+//Console.WriteLine("========================================="); 
+
+//Console.WriteLine("Digite 3 Nomes:"); 
+
+//var nome1 = Console.ReadLine();
+//var nome2 = Console.ReadLine();
+//var nome3 = Console.ReadLine();
+
+//Console.WriteLine($"""
+//    O valor que você digitou foi: {nome1}, {nome2}, {nome3}.
+//    """);
+
+//Console.WriteLine("Digite 3 Sobrenomes:");
+//var Sobrenome1 = Console.ReadLine();
+//var Sobrenome2 = Console.ReadLine();
+//var Sobrenome3 = Console.ReadLine();
+
+//Console.WriteLine($"""
+//    O valor que você digitou foi: {nome1 + Sobrenome1}, {nome2 + Sobrenome2}, {nome3 + Sobrenome3}.
+//    """);
+
+//Console.WriteLine("=== [Finalizando Sistema] ===");
+
+
+// Correção e melhorias da aula ao vivo
+Console.WriteLine("=== Bem vindo ao sistema de Cadastro de Nome e Sobrenome (2.0) ===");
+
+
+Console.WriteLine("Digite o primeiro nome:");
+var nome1 = Console.ReadLine();
+Console.WriteLine("Digite o segundo nome:");
+var nome2 = Console.ReadLine();
+Console.WriteLine("Digite o terceiro nome:");
+var nome3 = Console.ReadLine();
+
+
+Console.WriteLine("Digite o primeiro sobrenome");
+var Sobrenome1 = Console.ReadLine();
+Console.WriteLine("Digite o segundo sobrenome");
+var Sobrenome2 = Console.ReadLine();
+Console.WriteLine("Digite o terceiro sobrenome");
+var Sobrenome3 = Console.ReadLine();
+
+Console.WriteLine($"Nome 1: {nome1} {Sobrenome1}");
+Console.WriteLine($"Nome 2: {nome2} {Sobrenome2}");
+Console.WriteLine($"Nome 3: {nome3} {Sobrenome3}");
+
+Console.WriteLine("=== [Finalizando Sistema] ===");
+
