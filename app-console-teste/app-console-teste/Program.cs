cca527ec7a56ac5b059654dba5291b5f15cf3ae2
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-01-09 18:39:07 -0300

    Exercicio 3 logica

--- app-console-teste/app-console-teste/Program.cs
+++ app-console-teste/app-console-teste/Program.cs
@@ -1,5 +1,6 @@
 ﻿
-/*
+/* Anotações
+ * 
  mostre() // mostrar uma mensagem na tela
  leia() // ler algo que o usuario digita
  csharp é dinamico (var), forte tipagem,
@@ -9,27 +10,38 @@
  vc identificar string(classe ou primitivo) e tals é mais rapido para compilar ao inves de utilizar "var" pq espera a respota para identificar a varaivel 
  boa pratica de O.O camelCase para var
  w3schools.com site recomendado 
+ exatidao usar swtich case(processador mais rapido), não usar exatidao if/else(mais lento)
+ switch case para opções claras, if else range
+
 */
 
-// Aula 1- Logica
-//Console.WriteLine("========================================="); // mostre
-//Console.WriteLine("=== Bem vindo ai meu primeiro sistema ==="); // mostre
-//Console.WriteLine("========================================="); // mostre
+/* Aula 1- Logica
+Console.WriteLine("========================================="); // mostre
+Console.WriteLine("=== Bem vindo ai meu primeiro sistema ==="); // mostre
+Console.WriteLine("========================================="); // mostre
 
-//Console.WriteLine("Digite algo:"); // mostre()
-//var informacao = Console.ReadLine(); // leia() // vem acompanhado em armazenar(variavel) "setar algo"
+Console.WriteLine("Digite algo:"); // mostre()
+var informacao = Console.ReadLine(); // leia() // vem acompanhado em armazenar(variavel) "setar algo"
 
-//Console.WriteLine($"""
-//    O valor que você digitou foi: {informacao}
-//    """);
+Console.WriteLine($"""
+    O valor que você digitou foi: {informacao}
+    """);
 
-//Console.WriteLine("=== [Finalizando Sistema] ===");
+Console.WriteLine("=== [Finalizando Sistema] ===");
+*/
 
-// Aula 2 - Logica
-
+/* Aula 2 - Logica
 Console.WriteLine("Digite um número");
 double numero1 = Convert.ToDouble(Console.ReadLine());
 
 var resultado = numero1 * 1000;
 
 Console.WriteLine($"O resultado da operação é: {resultado}");
+*/
+
+/* Aula 3 - if/else
+ 
+*/    
+
+
+
---
+++ app-console-teste/exercicio-log-3/Program.cs
@@ -0,0 +1,111 @@
+﻿/*
+ saldo
+ descontar saldo 0,05%
+
+deseja sacar 
+se sim qual valor
+se sim for maior que saldo pedir para digitar novamente
+se n tchau
+
+ */
+
+//Console.WriteLine("=== Paybank V(1.0) ===");
+
+//Console.Write("Olá, digite seu nome: ");
+//var nome = Console.ReadLine();
+
+//Console.WriteLine($"Olá {nome}, digite o valor que irá depositar em R$:");
+//double saldo = Convert.ToDouble(Console.ReadLine());
+
+//var taxa = (saldo/100) * 0.05;
+//double valorComTaxa = saldo - taxa;
+
+//Console.WriteLine("======== Conta Inicial ========");
+//Console.WriteLine($"Saldo depositado em R$: {saldo} reais.");
+//Console.WriteLine($"Saldo com cobraçanda da taxa: {valorComTaxa} reais.");
+//Console.WriteLine("=============================\n");
+
+//Console.WriteLine($"Olá {nome}, dejesa realizar um saque? Responder S / N");
+//string? saque = Console.ReadLine();
+
+
+//Console.WriteLine("=== PayBank V(2.0) ===");
+
+//Console.Write("Olá, digite seu nome: ");
+//var nome = Console.ReadLine();
+
+//Console.WriteLine($"Olá {nome}, digite o valor que irá depositar em R$:");
+//double saldo = Convert.ToDouble(Console.ReadLine());
+
+//var taxa = (saldo / 100) * 0.05;
+//double valorComTaxa = saldo - taxa;
+
+//Console.WriteLine("======== conta inicial ========");
+//Console.WriteLine($"saldo depositado em r$: {valorComTaxa} reais.");
+
+//Console.WriteLine($"""
+//    Deseja realizar um saque?
+//    Digite 1 para Sim / 2 para Não
+//    """);
+//var escolha = Convert.ToInt16((Console.ReadLine()));
+
+//if(escolha == 1)
+//{
+//    Console.WriteLine("Qual valor desaja sacar?");
+//        var saque = Convert.ToDouble((Console.ReadLine()));
+
+//    if(saque <= valorComTaxa)
+//    {
+//        valorComTaxa -= saque;
+//        Console.WriteLine($"Saque realizado com sucesso, saldo atual R${valorComTaxa}");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"Desculpe valor superior ao saldo disponivel de R${valorComTaxa}");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Obrigado por utilizar os serviços do PayBank");
+//}
+
+
+Console.WriteLine("=== PayBank V(3.0) ===");
+
+Console.Write("Olá, digite seu nome: ");
+var nome = Console.ReadLine();
+
+Console.WriteLine($"Olá {nome}, digite o valor que irá depositar em R$:");
+double saldo = Convert.ToDouble(Console.ReadLine());
+
+var taxa = (saldo / 100) * 0.05;
+double valorComTaxa = saldo - taxa;
+
+Console.WriteLine("======== Saldo inicial ========");
+Console.WriteLine($"saldo depositado em r$: {valorComTaxa} reais.");
+
+Console.WriteLine($"""
+    Deseja realizar um saque?
+    Digite: S para Sim / N para Não
+    """);
+var escolha = Convert.ToString((Console.ReadLine()));
+
+if (escolha == 'S')
+{
+    Console.WriteLine("Qual valor desaja sacar?");
+    var saque = Convert.ToDouble((Console.ReadLine()));
+
+    if (saque <= valorComTaxa)
+    {
+        valorComTaxa -= saque;
+        Console.WriteLine($"Saque realizado com sucesso, saldo atual R${valorComTaxa}");
+    }
+    else
+    {
+        Console.WriteLine($"Desculpe valor superior ao saldo disponivel de R${valorComTaxa}");
+    }
+}
+else
+{
+    Console.WriteLine("Obrigado por utilizar os serviços do PayBank");
+}
