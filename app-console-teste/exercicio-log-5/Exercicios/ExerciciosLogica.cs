03e9e30f9ced98dac99be391d2361f11ba7e8d0b
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-01-30 07:15:13 -0300

    Refatorando Logica e implementando principios de O.O

--- app-console-teste/exercicio-log-5/Exercicios/ExerciciosLogica.cs
+++ app-console-teste/exercicio-log-5/Exercicios/ExerciciosLogica.cs
@@ -64,19 +64,19 @@ namespace Exercicios
 
         // ex 2
         /*
- Ex 2
- qtd comb
- preço litro
- mostrar esotque 
- valor de abastecimento
- qtd de litro abastecido
- valor por litro
+     Ex 2
+     qtd comb
+     preço litro
+     mostrar esotque 
+     valor de abastecimento
+     qtd de litro abastecido
+     valor por litro
 
- Melhorias 2.0
- pedir o nome X
- camelCase
- usabilidade do programa faltou
-*/
+     Melhorias 2.0
+     pedir o nome X
+     camelCase
+     usabilidade do programa faltou
+    */
 
         //Console.WriteLine("=== Sistema Posto de combustivel V(1.0) ===");
 
@@ -132,15 +132,15 @@ namespace Exercicios
 
         //Ex 3
         /*
- saldo
- descontar saldo 0,05%
+        saldo
+        descontar saldo 0,05%
 
-deseja sacar 
-se sim qual valor
-se sim for maior que saldo pedir para digitar novamente
-se n tchau
+       deseja sacar 
+       se sim qual valor
+       se sim for maior que saldo pedir para digitar novamente
+       se n tchau
 
- */
+        */
 
         //Console.WriteLine("=== Paybank V(1.0) ===");
 
@@ -285,6 +285,277 @@ se n tchau
         //for (int i = 1; i <= multiplicacao; i++)
         //{
         //    Console.WriteLine($"{multiplicador} X {i} = {multiplicador * i}");
+        //}
+
+        // ex 6 minha versao
+        //while (true)
+        //{
+        //    Console.Clear();
+
+        //    Console.WriteLine("""
+        //    ================[ Seja Bem vindo a Empresa Lina V(1.0) ]=================
+        //    O que você deseja fazer ?
+        //    1 - Cadastrar o Cliente
+        //    2 - Ver conta conrrente
+        //    3 - Fazer Crebito em conta
+        //    4 - Fazer Dedito em conta
+        //    5 - Sair do Sistema
+        //    """);
+
+        //    var opcao = Console.ReadLine()?.Trim();
+        //    Console.Clear();
+
+        //    bool sair = false;
+
+
+        //    switch (opcao)
+        //    {
+        //        case "1":
+
+        //            Console.Write("Olá, digite seu nome: ");
+        //            var nome = Console.ReadLine();
+
+
+
+        //            break;
+        //        case "2":
+        //            Console.WriteLine($"Olá, digite o valor que irá depositar em R$:");
+        //            double saldo = Convert.ToDouble(Console.ReadLine());
+
+
+        //            Console.WriteLine("======== Saldo inicial ========");
+        //            Console.WriteLine($"saldo depositado em r$: {saldo} reais.");
+
+        //            Console.Clear();
+        //            break;
+
+        //            case "3":
+        //            Console.WriteLine($"""
+        //                Deseja realizar um Crédito?
+        //                Digite: S para Sim / N para Não
+        //                """);
+
+        //            Console.Clear();
+
+        //            break;
+        //            case "4":
+        //            Console.WriteLine($"""
+        //                Deseja realizar um Dédito?
+        //                Digite: S para Sim / N para Não
+        //                """);
+
+        //            break;
+        //        case "5":
+        //            sair = true;
+        //            break;
+        //        default:
+        //            Console.WriteLine("Opção inválida");
+        //            break;
+        //    }
+
+        //    if (sair) break;
+
+        //    Thread.Sleep(4000);
+        //}
+
+        //using System;
+
+        //while (true)
+        //{
+        //    Console.Clear();
+
+        //    Console.WriteLine("""
+        //    ================[ Seja Bem vindo a Empresa Lina V(2.0) ]=================
+        //    O que você deseja fazer ?
+        //    1 - Cadastrar o Cliente
+        //    2 - Ver conta conrrente
+        //    3 - Fazer Crebito em conta
+        //    4 - Fazer Dedito em conta
+        //    5 - Sair do Sistema
+        //    """);
+
+        //    var opcao = Console.ReadLine()?.Trim();
+        //    Console.Clear();
+        //    bool sair = false;
+
+
+        //    switch (opcao)
+        //    {
+        //        case "1":
+        //            var id = Guid.NewGuid();
+        //            Console.WriteLine("O Guid é " + id);
+
+        //            Console.Write("Olá Cliente, digite seu nome: \n");
+        //            var nome = Console.ReadLine();
+
+        //            Console.Write($"Olá {nome}, digite seu telefone: Ex (00) 00000 0000 \n");
+        //            var telefone = Console.ReadLine();
+
+        //            Console.Write("Digite seu email: \n");
+        //            var email = Console.ReadLine();
+
+        //            Console.WriteLine("==== Bem vindo a Empresa Lina====");
+        //            Console.WriteLine(nome);
+        //            Console.WriteLine(telefone);
+        //            Console.WriteLine(email);
+        //            Console.WriteLine(id);
+
+        //            break;
+        //        case "2":
+        //          //  Console.WriteLine($"Olá, digite o valor que irá depositar em R$:");
+        //          //  double saldo = Convert.ToDouble(Console.ReadLine());
+
+
+        //           // Console.WriteLine("======== Saldo inicial ========");
+        //           // Console.WriteLine($"saldo depositado em r$: {saldo} reais.");
+
+        //            //Console.Clear();
+        //            break;
+
+        //        case "3":
+        //         //   Console.WriteLine($"""
+        //            //    Deseja realizar um Crédito?
+        //           //     Digite: S para Sim / N para Não
+        //           //     """);
+
+        //            Console.Clear();
+
+        //            break;
+        //        case "4":
+        //           // Console.WriteLine($"""
+        //           //     Deseja realizar um Dédito?
+        //           //     Digite: S para Sim / N para Não
+        //           //     """);
+
+        //            break;
+        //        case "5":
+        //            sair = true;
+        //            break;
+        //        default:
+        //            Console.WriteLine("Opção inválida");
+        //            break;
+        //    }
+
+        //    if (sair) break;
+
+        //    Thread.Sleep(4000);
+        //}
+
+        //using System;
+        //using System.ComponentModel;
+
+        //List<string[]> list = new List<string[]>();
+
+        //while (true)
+        //{
+        //    Console.Clear();
+
+        //    Console.WriteLine("""
+        //    ================[ Seja Bem vindo a Empresa Lina V(3.0) ]=================
+        //    O que você deseja fazer ?
+        //    1 - Cadastrar o Cliente
+        //    2 - Ver conta conrrente
+        //    3 - Fazer Crebito em conta
+        //    4 - Fazer Dedito em conta
+        //    5 - Sair do Sistema
+        //    """);
+
+        //    var opcao = Console.ReadLine()?.Trim();
+        //    Console.Clear();
+        //    bool sair = false;
+
+
+        //    switch (opcao)
+        //    {
+        //        case "1":
+        //            Console.Clear();
+        //            cadastrarCliente();
+
+        //            break;
+        //        case "2":
+
+        //            Console.Clear();
+        //            cadastrarSaldo();
+
+        //            break;
+
+        //        case "3":
+
+        //            Console.Clear();
+        //            credito();
+
+        //            break;
+        //        case "4":
+        //            // Console.WriteLine($"""
+        //            //     Deseja realizar um Dédito?
+        //            //     Digite: S para Sim / N para Não
+        //            //     """);
+
+        //            break;
+        //        case "5":
+        //            sair = true;
+        //            break;
+        //        default:
+        //            Console.WriteLine("Opção inválida");
+        //            break;
+        //    }
+
+        //    void cadastrarCliente()
+        //    {
+        //        var id = Guid.NewGuid();
+
+        //        Console.Write("Olá Cliente, digite seu nome: \n");
+        //        var nome = Console.ReadLine();
+
+        //        Console.Write($"Olá {nome}, digite seu telefone: Ex (00) 00000 0000 \n");
+        //        var telefone = Console.ReadLine();
+
+        //        Console.Write("Digite seu email: \n");
+        //        var email = Console.ReadLine();
+
+
+        //        string[] cliente = new string[4];
+
+        //        cliente[0] = id.ToString();
+        //        cliente[1] = nome != null ? nome : "[Sem Nome]";
+        //        cliente[2] = telefone != null ? telefone : "[Sem Telefone ]";
+        //        cliente[3] = email != null ? email : "[Sem Email]";
+
+
+        //        mensagem($"""Cliente {id} - {nome} cadastrado com sucesso.""");
+        //    }
+
+        //    if (sair) break;
+
+        //    Thread.Sleep(4000);
+        //}
+
+        //void cadastrarSaldo()
+        //{
+        //    Console.WriteLine($"Olá, digite o valor que irá depositar em R$:");
+        //    double saldo = Convert.ToDouble(Console.ReadLine());
+
+        //    double[] deposito = new double[1];
+
+        //    deposito[0] = saldo;
+
+        //    Console.WriteLine($"Seu saldo é de R$: {saldo}");
+
+        //}
+
+
+        //void credito()
+        //{
+        //   foreach(double valorDeposito in saldo)
+        //    Console.WriteLine($"Seu valor em conta é de R$: {saldo}");
+        //}
+
+
+        //void mensagem(string msg)
+        //{
+        //    Console.Clear();
+        //    Console.WriteLine(msg);
+        //    Thread.Sleep(2000);
         //}
     }
 }
---
+++ app-console-teste/exercicio-log-5/Servicos/ContaCorrenteServico.cs
@@ -0,0 +1,38 @@
+﻿using Logica.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.Servicos
+{
+    class ContaCorrenteServico
+    {
+        private ContaCorrenteServico() { }
+
+        private ContaCorrenteServico instancia;
+
+        private List<ContaCorrente> contaCorrente = new List<ContaCorrente>();
+
+        public List<ContaCorrente> extratoCliente(string idCliente)
+        {
+            var contaCorreteCliente = contaCorrente.FindAll(cc => cc.IdCliente == idCliente);
+            if (contaCorreteCliente.Count == 0) return new List<ContaCorrente>();
+
+            return contaCorreteCliente;
+        }
+
+        public double saldoCliente(string idCliente, List<ContaCorrente>? contaCorreteCliente = null)
+        {
+            if (contaCorreteCliente == null)
+                contaCorreteCliente = extratoCliente(idCliente);
+
+            if (contaCorreteCliente.Count == 0) return 0;
+
+            return Convert.ToDouble(contaCorreteCliente.Sum(cc => cc.Valor));
+        }
+
+
+    }
+}
