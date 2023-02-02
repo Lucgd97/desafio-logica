92fc1cae9b7c598d57a673f1f6f117e88915c457
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-02-02 20:05:30 -0300

    Correcao do programa e organizacao de pastas

---
+++ Programa-Desafio-Logica/Exercicio/Anotacoes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_Desafio_Logica.Exercicio
+{
+    internal class Desafios
+    {
+    }
+}
