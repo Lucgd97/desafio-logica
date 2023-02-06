424887c458cdf0fbfb23b4fb8c8e52d271f0e89d
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-02-06 19:11:52 -0300

    Correcao de bug

--- programa/programa.test/Class1.cs
+++ programa/programa.test/Class1.cs
@@ -1,5 +1,10 @@
-﻿namespace programa.test;
+﻿using Programa.Models;
+
+namespace programa.test;
 public class Class1
 {
+    public Class1(){
+        var cliente = new Cliente();
 
-}
+    }
+}    
--- programa/programa/Models/Cliente.cs
+++ programa/programa/Models/Cliente.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Programa.Models
 {
-    struct Cliente
+    public struct Cliente
     {
         public string Id { get; set; }
         public string Nome { get; set; }
