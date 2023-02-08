cc869ff84f5a572c266c41404c5dfd40d07117f9
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-02-08 20:45:28 -0300

    Realizando teste no programa

--- programa/programa.test/Models/ClienteTest.cs
+++ programa/programa.test/Models/ClienteTest.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Programa.Models;
 
-namespace Programa.Test.Models
+namespace Programa.Test.Models;
+
+[TestClass]
+public class ClienteTest
 {
-    public class ClienteTest
-    {
+    [TestMethod]
+    public void TestandoPropriedadesDaClasse(){
         
+        var cliente = new Cliente();
+        // tstar set (public or private)
+        cliente.Id = "23432123";
+        cliente.Nome = "Marcela";
+        cliente.Email = "ma@teste.com";
+        cliente.Telefone = "(11)12345-1234";
+
+        // testar get (public or private)
+        Assert.AreEqual("23432123", cliente.Id);
+        Assert.AreEqual("Marcela", cliente.Nome);
+        Assert.AreEqual("ma@teste.com", cliente.Email);
+        Assert.AreEqual("(11)12345-1234", cliente.Telefone);
     }
 }
