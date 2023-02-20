b20d4ebb8420d7619482c1d36cb853c0aad53b12
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-02-20 07:09:15 -0300

    Implementando metodo atualizarPropriedades para refatorar metodo Salvar

--- programa/programa/Infra/JsonDriver.cs
+++ programa/programa/Infra/JsonDriver.cs
@@ -23,6 +23,11 @@ public class JsonDriver<T> : IPersistencia<T>
     public async Task<T> BuscarPorId(string id)
     {
         var lista = await Todos();
+        return buscaListaId(lista, id);
+    }
+
+    private T buscaListaId(List<T> lista, string id)
+    {
         return lista.Find(o => o.GetType().GetProperty("Id").GetValue(o).ToString() == id);
     }
 
@@ -33,13 +38,40 @@ public class JsonDriver<T> : IPersistencia<T>
 
     public async Task Salvar(T objeto)
     {
+        if(objeto == null) return;
+
         var lista = await Todos();
+
+        var id = objeto.GetType().GetProperty("Id")?.GetValue(objeto)?.ToString();
+        if(string.IsNullOrEmpty(id)) return;
+
+        var objLista = buscaListaId(lista, id);
+        if(objLista == null) lista.Add(objeto);
+        else
+        {
+            atualizaPropriedades(ref objeto, ref objLista);
+        }
+
         lista.Add(objeto);
 
         string jsonString = JsonSerializer.Serialize(lista);
         
         var nome = typeof(T).Name.ToLower();
         await File.WriteAllTextAsync($"{this.GetLocalGravacao()}/{nome}s.json", jsonString);
+    }
+
+    private void atualizaPropriedades(ref T objetoDe, ref T objListaPara)
+    {
+        if(objetoDe == null || objListaPara == null) return;
+
+        foreach(var propDe in objetoDe.GetType().GetProperties())
+        {
+            var propPara = objListaPara.GetType().GetProperty(propDe.Name);
+            if(propPara != null)
+            {
+                propPara.SetValue(objListaPara, propDe.GetValue(objetoDe));
+            }
+        }
     }
 
     public async Task<List<T>> Todos()
