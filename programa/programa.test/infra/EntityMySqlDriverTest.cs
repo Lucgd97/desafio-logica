bc4c396e3aa3f1c6c422bf741525527a39e6075f
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-03-12 12:37:22 -0300

    Implementando Drivers Entity e MongoDb

---
+++ programa/programa.test/infra/EntityMySqlDriverTest.cs
@@ -0,0 +1,149 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Programa.Models;
+
+namespace Programa.Infra;
+
+[TestClass]
+public class EntityMySqlDriverTest
+{
+
+    public EntityMySqlDriverTest()
+    {
+        var caminho = Environment.GetEnvironmentVariable("LOCAL_GRAVACAO_TEST_DESAFIO_DOTNET7_MYSQL") ?? "Server=localhost;Database=dotnet7_driver;Uid=root;Pwd=Budboa@123;";
+        this.caminhoArquivoTest = caminho;
+    }
+
+    private string caminhoArquivoTest;
+
+    [TestInitialize()]
+    public async Task Startup()
+    {
+       await new EntityMySqlDriver<Cliente>(this.caminhoArquivoTest).ExcluirTudo();
+       await new EntityMySqlDriver<ContaCorrente>(this.caminhoArquivoTest).ExcluirTudo();
+    }
+    
+    [TestMethod]
+    public async Task TestandoDriverJsonParaClientes()
+    {   
+        
+        var jsonDriver = new EntityMySqlDriver<Cliente>(this.caminhoArquivoTest);
+
+        var cliente = new Cliente()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Nome = "Danilo",
+            Email = "danilo@teste.com",
+            Telefone = "(11)12345-1234"
+        };
+
+        await jsonDriver.Salvar(cliente);
+
+        var existe = File.Exists(this.caminhoArquivoTest + "/clientes.json");
+    }
+
+    [TestMethod]
+    public async Task TestandoDriverJsonParaContaCorrente()
+    {  
+        var jsonDriver = new EntityMySqlDriver<ContaCorrente>(this.caminhoArquivoTest);
+
+        var contaCorrente = new ContaCorrente()
+        {
+            Id = Guid.NewGuid().ToString(),
+            IdCliente = Guid.NewGuid().ToString(),
+            Valor = 200,
+            Data = DateTime.Now
+        };
+
+        await jsonDriver.Salvar(contaCorrente);
+
+        var existe = File.Exists(this.caminhoArquivoTest + "/contascorrentes.json");
+    }
+
+    [TestMethod]
+    public async Task TestandoBuscaDeTodasAsEntidades()
+    {      
+        var jsonDriver = new EntityMySqlDriver<ContaCorrente>(this.caminhoArquivoTest);
+             
+        var contaCorrente = new ContaCorrente()
+        {
+            Id = Guid.NewGuid().ToString(),
+            IdCliente = Guid.NewGuid().ToString(),
+            Valor = 200,
+            Data = DateTime.Now
+        };
+
+        await jsonDriver.Salvar(contaCorrente);
+
+        var todos = await jsonDriver.Todos();
+
+        Assert.IsTrue(todos.Count > 0);
+    }
+
+    [TestMethod]
+    public async Task TestandoBsucaPorId()
+    {      
+        var jsonDriver = new EntityMySqlDriver<Cliente>(this.caminhoArquivoTest);
+             
+        var cliente = new Cliente(){
+            Id =Guid.NewGuid().ToString(),
+            Nome = "Danilo " + DateTime.Now,
+            Email = "danilo@teste.com",
+            Telefone = "(16)12345-1234"
+        };
+
+        await jsonDriver.Salvar(cliente);
+
+        var clienteDb = await jsonDriver.BuscarPorId(cliente.Id);
+        
+        Assert.AreEqual(cliente.Nome, clienteDb?.Nome);
+    }
+
+    [TestMethod]
+    public async Task TestandoAlterecaoDeEntidade()
+    {      
+        var jsonDriver = new EntityMySqlDriver<Cliente>(this.caminhoArquivoTest);
+             
+        var cliente = new Cliente(){
+            Id =Guid.NewGuid().ToString(),
+            Nome = "Danilo",
+            Email = "danilo@teste.com",
+            Telefone = "(16)12345-1234"
+        };
+
+        await jsonDriver.Salvar(cliente);
+
+        cliente.Nome = "Danilo Santos";
+
+        await jsonDriver.Salvar(cliente);
+
+        var clienteDb = await jsonDriver.BuscarPorId(cliente.Id);
+        
+        Assert.AreEqual("Danilo Santos", clienteDb?.Nome);
+    }
+
+    [TestMethod]
+    public async Task TestandoExcluirEntidades()
+    {      
+        var jsonDriver = new EntityMySqlDriver<ContaCorrente>(this.caminhoArquivoTest);
+             
+        var contaCorrente = new ContaCorrente()
+        {
+            Id = Guid.NewGuid().ToString(),
+            IdCliente = Guid.NewGuid().ToString(),
+            Valor = 200,
+            Data = DateTime.Now
+        };
+
+        await jsonDriver.Salvar(contaCorrente);
+
+        var objDb = await jsonDriver.BuscarPorId(contaCorrente.Id);
+        Assert.IsNotNull(objDb);
+        Assert.IsNotNull(objDb?.Id);
+
+        await jsonDriver.Excluir(contaCorrente);
+        
+        var objDb2 = await jsonDriver.BuscarPorId(contaCorrente.Id);        
+        Assert.IsNull(objDb2);
+        
+    }
+}
